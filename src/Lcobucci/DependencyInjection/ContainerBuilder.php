2a8a59512e7b458fe34fd8d91a80adff212ae36d
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2013-04-15 02:14:53 -0300

    Project migrated from lcobucci/common

---
+++ src/Lcobucci/DependencyInjection/ContainerBuilder.php
@@ -0,0 +1,43 @@
+<?php
+namespace Lcobucci\DependencyInjection;
+
+/**
+ * The dependency injection container builder
+ *
+ * @author Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
+ */
+interface ContainerBuilder
+{
+    /**
+     * Creates an instance of the builder and build the container for given file
+     *
+     * @param string $file
+     * @param array $path
+     * @param string $baseClass
+     * @param string $cacheDirectory
+     * @return \Symfony\Component\DependencyInjection\ContainerBuilder
+     */
+    public static function build(
+        $file,
+        array $path = array(),
+        $baseClass = null,
+        $cacheDirectory = null
+    );
+
+    /**
+     * Creates a new object
+     *
+     * @param string $baseClass
+     * @param string $cacheDirectory
+     */
+    public function __construct($baseClass = null, $cacheDirectory = null);
+
+    /**
+     * Create a dump file (if needed) and load the container
+     *
+     * @param string $file
+     * @param array $path
+     * @return \Symfony\Component\DependencyInjection\ContainerBuilder
+     */
+    public function getContainer($file, array $path = array());
+}
