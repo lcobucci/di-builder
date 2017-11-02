52f49c6567cadfa5e2fba32326c36f78fec92f02
Luís Cobucci <lcobucci@gmail.com>
2017-11-02 14:11:58 +0100

    Add compiler pass that turns services public
    Which is mandatory in order to create containers where all services
    can be overriden with no effort. Is quite useful for integration tests.

---
+++ src/Testing/MakeServicesPublic.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Lcobucci\DependencyInjection\Testing;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+final class MakeServicesPublic implements CompilerPassInterface
+{
+    /**
+     * You can modify the container here before it is dumped to PHP code.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container)
+    {
+        foreach ($container->getDefinitions() as $definition) {
+            $definition->setPublic(true);
+        }
+
+        foreach ($container->getAliases() as $alias) {
+            $alias->setPublic(true);
+        }
+    }
+}
